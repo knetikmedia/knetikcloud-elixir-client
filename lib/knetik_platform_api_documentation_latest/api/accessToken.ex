b7c4ab3e3d2f8b9f4bd85153ce63cdaec82cb8cd
stoutfiles <shawn.stout@knetik.com>
2017-08-15 13:51:20 -0400

    JSAPI Elixir API update

--- lib/knetik_platform_api_documentation_latest/api/accessToken.ex
+++ lib/knetik_platform_api_documentation_latest/api/accessToken.ex
@@ -13,13 +13,13 @@ defmodule KnetikPlatformAPIDocumentationLatest.Api.AccessToken do
 
   
   """
-  def get_o_auth_token(grant_type, client_id, client_secret, username, password) do
+  def get_o_auth_token(grant_type, client_id, client_secret, username, password, token, refresh_token) do
     method = [method: :post]
     url = [url: "/oauth/token"]
     query_params = []
     header_params = []
     body_params = []
-    form_params = [body: Enum.map_join([{:"grant_type", grant_type}, {:"client_id", client_id}, {:"client_secret", client_secret}, {:"username", username}, {:"password", password}], "&", &("#{elem(&1, 0)}=#{elem(&1, 1)}"))]
+    form_params = [body: Enum.map_join([{:"grant_type", grant_type}, {:"client_id", client_id}, {:"client_secret", client_secret}, {:"username", username}, {:"password", password}, {:"token", token}, {:"refresh_token", refresh_token}], "&", &("#{elem(&1, 0)}=#{elem(&1, 1)}"))]
     params = query_params ++ header_params ++ body_params ++ form_params
     opts = []
     options = method ++ url ++ params ++ opts
